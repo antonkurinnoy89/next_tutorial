9dea53515864f9670b1e1ba072a6341d8c54a113
Anton Kurinnoy <antonkurinnoy89@gmail.com>
2024-07-15 12:02:24 +0300

    chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Customers(): JSX.Element {
   return <div>Customers Page</div>;
 }
